eb71dd6d57e9745c77cf9ac605439b8a01ca76a9
Robb Shecter <robb@weblaws.org>
2016-08-28 15:26:28 -0700

    refactoring

--- lib/string_ops.ex
+++ lib/string_ops.ex
@@ -1,3 +1,5 @@
+import String
+
 defmodule StringOps do
 
   def clean_up(title) when is_bitstring(title) do
@@ -9,25 +11,21 @@ defmodule StringOps do
   end
 
 
-  def fixWhitespace(s),  do: String.replace(s, "\n", " ")
+  def fixWhitespace(s),  do: replace(s, "\n", " ")
 
 
-  def fixHyphenation(s), do: String.replace(s, "- ", "")
+  def fixHyphenation(s), do: replace(s, "- ", "")
 
 
   def splitIntoSentences(a_string) do
     a_string
-      |> String.split(". ")
+      |> split(". ")
       |> Enum.map(&ensure_ends_with_period/1)
   end
 
 
-  def ensure_ends_with_period(s) when is_bitstring(s) do
-    if String.ends_with?(s, ".") do
-      s
-    else
-      s <> "."
-    end
+  def ensure_ends_with_period(sentence) do
+    sentence <> if ends_with?(sentence, "."), do: "", else: "."
   end
 
 end
