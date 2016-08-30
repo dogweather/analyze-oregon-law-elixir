4200ea475362ab269a330e4d6be5f7602990b3eb
Robb Shecter <robb.shecter@gsa.gov>
2016-08-29 17:39:11 -0700

    Cleanup

--- lib/string_ops.ex
+++ lib/string_ops.ex
@@ -2,7 +2,7 @@ import String
 
 defmodule StringOps do
 
-  def clean_up(title) when is_bitstring(title) do
+  def clean_up(title) do
     title
       |> fixWhitespace
       |> fixHyphenation
@@ -10,19 +10,15 @@ defmodule StringOps do
       |> List.first
   end
 
-
   def fixWhitespace(s),  do: replace(s, "\n", " ")
 
-
   def fixHyphenation(s), do: replace(s, "- ", "")
-
 
   def splitIntoSentences(a_string) do
     a_string
       |> split(". ")
       |> Enum.map(&ensure_ends_with_period/1)
   end
-
 
   def ensure_ends_with_period(sentence) do
     sentence <> if ends_with?(sentence, "."), do: "", else: "."
