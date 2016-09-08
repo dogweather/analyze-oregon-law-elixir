881382339073f3b070dad6836d81c5ed06566024
Robb Shecter <robb.shecter@gsa.gov>
2016-09-08 00:21:50 -0700

    make_bill/1

--- lib/amendment.ex
+++ lib/amendment.ex
@@ -1,3 +1,13 @@
+defmodule Bill do
+  defstruct bill_type: nil, bill_number: nil
+end
+
 defmodule Amendment do
   def summary?(sentence), do: String.starts_with?(sentence, "Relating to")
+
+  # Create a %Bill from a string in short form, e.g. "HB 1234".
+  def make_bill(citation) when is_bitstring(citation) do
+    [chamber, number] = String.split citation
+    %Bill{ bill_type: chamber, bill_number: String.to_integer(number) }
+  end
 end
--- test/amendment_test.exs
+++ test/amendment_test.exs
@@ -7,4 +7,14 @@ defmodule AmendmentTest do
     summary_hb_4047 = "Relating to speed limits on highways that traverse state lines; creating new provisions; amending ORS 811.111; and declaring an emergency."
     assert summary?(summary_hb_4047)
   end
+
+  test "make_bill can parse a typical house bill" do
+    bill = make_bill("HB 4047")
+    assert %Bill{ bill_type: "HB", bill_number: 4047 } = bill
+  end
+
+  test "make_bill can parse a typical senate bill" do
+    bill = make_bill("SB 1532")
+    assert %Bill{ bill_type: "SB", bill_number: 1532 } = bill
+  end
 end
